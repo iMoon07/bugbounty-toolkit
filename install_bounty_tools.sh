02edf2280bf7b5416252ac2cae054a0bc6b71d81
rootbakar <43517550+rootbakar@users.noreply.github.com>
2024-10-17 10:50:27 +0700

    Update install_bounty_tools.sh

--- install_bounty_tools.sh
+++ install_bounty_tools.sh
@@ -121,6 +121,7 @@ go install -v github.com/tomnomnom/anew@latest
 go install -v github.com/tomnomnom/unfurl@latest
 go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
 go install -v github.com/PentestPad/subzy@latest
+go install -v github.com/takshal/freq@latest
 
 # Copying Go tools to /usr/local/bin
 print_message "Copying Go tools to /usr/local/bin..."
